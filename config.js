31291185c45cb861cb0f267e38eedd06cea6019e
Teng <tenghuang1996@gmail.com>
2024-04-19 13:51:49 -0700

    updated server link

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-bc47686f-1dee-412a-9cab-6be66d03e4dd.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-522cdeda-c3bb-4aa7-bef1-e56f6fbfa741.containerhub.tripleten-services.com"
 }
